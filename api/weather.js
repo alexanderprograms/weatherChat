cc9cfa900d41723f185f275f3572023af860a4a6
alexanderprograms <alexanderryan98@gmail.com>
2026-04-07 06:21:47 +0100

    Added additional debugging output

--- api/weather.js
+++ api/weather.js
@@ -53,6 +53,7 @@ export default async function handler(req, res) {
     });
 
   } catch (err) {
+    console.error('Weather API error:', err);
     return res.status(500).json({ error: err.message });
   }
 }
